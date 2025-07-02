c3e261ed9a9cbcc6450efc215c431c49405ea0c0
Tetsu45 <htetmyetkyaw163@gmail.com>
2025-07-02 13:57:33 +0900

    Greetings added

--- RollingTwoDices.py
+++ RollingTwoDices.py
@@ -1,9 +1,14 @@
 import random
+def greet():
+    print("What is your name?")
+    name = input()
+    print(f"Hello, {name}!")
 def roll_two_dices():
     die1 = random.randint(1, 6)
     die2 = random.randint(1, 6)
     return (die1, die2)
 def main():
+    greet()
     die1, die2 = roll_two_dices()
     print(f"Die 1: {die1}")
     print(f"Die 2: {die2}")
