633bb58bd682632ca51515136a7555e9889ccd62
Tetsu45 <htetmyetkyaw163@gmail.com>
2025-07-02 14:23:34 +0900

    Won/lost message added

--- RollingTwoDices.py
+++ RollingTwoDices.py
@@ -1,4 +1,6 @@
 import random
+def CheckIfGreaterThan7(die1, die2):
+    return (die1 + die2) > 7
 def roll_two_dices():
     die1 = random.randint(1, 6)
     die2 = random.randint(1, 6)
@@ -8,5 +10,9 @@ def main():
     print(f"Die 1: {die1}")
     print(f"Die 2: {die2}")
     print(f"Total: {die1 + die2}")
+    if CheckIfGreaterThan7(die1, die2):
+        print("You won")
+    else:
+        print("You lost")
 if __name__ == "__main__":
     main()
