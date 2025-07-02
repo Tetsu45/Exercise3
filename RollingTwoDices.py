dc462d45922c0a740639344f5e7ab7b7b189f903
Tetsu45 <htetmyetkyaw163@gmail.com>
2025-07-02 14:35:11 +0900

    Won/lost message modified

--- RollingTwoDices.py
+++ RollingTwoDices.py
@@ -5,19 +5,20 @@ def greet():
     print("What is your name?")
     name = input()
     print(f"Hello, {name}!")
+    return name
 def roll_two_dices():
     die1 = random.randint(1, 6)
     die2 = random.randint(1, 6)
     return (die1, die2)
 def main():
-    greet()
+    name = greet()
     die1, die2 = roll_two_dices()
     print(f"Die 1: {die1}")
     print(f"Die 2: {die2}")
     print(f"Total: {die1 + die2}")
     if CheckIfGreaterThan7(die1, die2):
-        print("You won")
+        print(f"{name} won!")
     else:
-        print("You lost")
+        print(f"{name} lost!")
 if __name__ == "__main__":
     main()
